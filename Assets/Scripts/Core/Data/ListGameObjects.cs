199e505dfaa015e40d121d73b474ecc09d81e5ad
Станислав Хозяенок <st.hoz.kem@gmail.com>
2025-04-05 13:03:48 +0700

    Добавлены core interface

---
+++ Assets/Scripts/Core/Data/ListGameObjects.cs
@@ -0,0 +1,19 @@
+﻿using Assets.Scripts.Core.Interfaces.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scenes.Econom_cards.Data
+{
+    public class ListGameObjects :  MonoBehaviour ,IListGameObjects
+    {
+        [Header("Список объектов")]
+        [Rename("Список")]
+        [Tooltip("Список")]
+        public List<GameObject> InputGameObjectsList;
+        public List<GameObject> GameObjectsList { get => InputGameObjectsList; set => InputGameObjectsList = value; }
+    }
+}
